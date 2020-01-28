f491a52c6992119f09f4726142f942c0e8244a34
Saurabhdimri06 <dimrisaurabh06@gmail.com>
2020-01-28 17:48:29 +0530

    Dashboard body

---
+++ src/components/body.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+class Body extends React.Component{
+    render(){
+        return(
+            <div className="db_body">
+                This is the place where the cards will lie              
+            </div>
+        )
+    }
+}
+
+export default Body

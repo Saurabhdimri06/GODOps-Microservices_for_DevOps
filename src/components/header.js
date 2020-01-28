22584d481f117e9b4a53f9a6619963b5408971f2
Saurabhdimri06 <dimrisaurabh06@gmail.com>
2020-01-28 17:48:47 +0530

    Header for the dasboard

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Header extends React.Component{
+    render() {
+        return(
+            <div className="header_main">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Header

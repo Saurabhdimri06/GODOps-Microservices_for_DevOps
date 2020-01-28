99641b1e7e38a1d37e8107a4856474a4e5f2cab2
Saurabhdimri06 <dimrisaurabh06@gmail.com>
2020-01-28 18:54:00 +0530

    Body template (card)

--- src/components/body.js
+++ src/components/body.js
@@ -5,7 +5,14 @@ class Body extends React.Component{
     render(){
         return(
             <div className="db_body">
-                This is the place where the cards will lie              
+                This is the place where the cards will lie 
+                <br />
+                <div className="main_cards">
+                    This is the element for card
+                </div>
+                <div className="main_cards">
+                    Second card for SonarQube
+                </div>
             </div>
         )
     }

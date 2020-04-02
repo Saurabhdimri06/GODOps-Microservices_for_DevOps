1b6791eef42db132df531b0b97c752395893b866
Saurabhdimri06 <dimrisaurabh06@gmail.com>
2020-04-02 10:45:39 +0530

    Main dashboard body structure

--- src/components/body.js
+++ src/components/body.js
@@ -5,6 +5,11 @@ class Body extends React.Component{
     render(){
         return(
             <div className="db_body">
+                <form className="inputform">
+                    <input type="text" id="basic-url" placeholder="Github Repo Link" />
+                    <button>Submit</button>
+                </form>
+                <br />
                 This is the place where the cards will lie 
                 <br />
                 <div className="main_cards">
